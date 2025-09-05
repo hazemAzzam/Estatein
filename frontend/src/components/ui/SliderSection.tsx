0e113e032796cac6e0a0075fd55b33b455b60b23
hazemAzzam <61450444+hazemAzzam@users.noreply.github.com>
2025-09-05 19:16:05 +0300

    feat: enhance SliderSection component with layout flexibility
    - Added layout prop to SliderSection for customizable column or row display.

--- frontend/src/components/ui/SliderSection.tsx
+++ frontend/src/components/ui/SliderSection.tsx
@@ -1,4 +1,5 @@
 "use client";
+import clsx from "clsx";
 import { ArrowLeft, ArrowRight } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
@@ -8,6 +9,7 @@ type SectionProps = {
   title: string;
   description: string;
   action_name?: string;
+  layout?: "column" | "row";
   children?: React.ReactNode;
 };
 
@@ -16,13 +18,19 @@ export default function Section({
   description,
   action_name,
   children,
+  layout = "column",
 }: SectionProps) {
   return (
     <div className="w-full h-fit  flex items-center justify-center px-5 py-5">
       <div className="relative w-full max-w-[1400px] h-full bg-gray-08">
         <div className="w-full h-full flex flex-col items-start justify-center">
           <Image src="/assets/stars.svg" alt="" width={100} height={50} />
-          <div className="px-5 w-full flex flex-col gap-10">
+          <div
+            className={clsx(
+              "px-5 w-full flex flex-col gap-10",
+              layout === "row" && "flex-row"
+            )}
+          >
             <div className="flex flex-row flex-wrap gap-5 w-full justify-between py-5">
               <div className="flex flex-col gap-5 max-w-[1000px] ">
                 <h2 className="text-5xl">{title}</h2>
